62d73b5cc35632ab7372776701552ac6bd3af793
VMz69 <viemvema@gmail.com>
2025-11-27 00:36:27 -0600

    CRUD de empleados

---
+++ FrontendDASALUD/Models/Rol-EspecialidadDtos.cs
@@ -0,0 +1,15 @@
+﻿namespace FrontendDASALUD.Models
+{
+    public class RolDto
+    {
+        public int IdRol { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+    }
+
+    public class EspecialidadDto
+    {
+        public int IdEspecialidad { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+    }
+
+}
