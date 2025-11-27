649952adefbdc68e6bcfb0f6656dec0aeec5b677
Gerson Borja <hipersocial08@gmail.com>
2025-11-27 03:53:26 -0600

    Se agregaron las vistas CRUD listar, agregar, editar y eliminar consumiendo los endpoints de paciente

---
+++ FrontendDASALUD/Models/PacienteDto.cs
@@ -0,0 +1,22 @@
+﻿namespace FrontendDASALUD.Models
+{
+    public class PacienteDtos
+    {
+        public int IdPaciente { get; set; }
+        public string Nombres { get; set; } = string.Empty;
+        public string Apellidos { get; set; } = string.Empty;
+        public string FullName => $"{Nombres} {Apellidos}".Trim();
+        public string Dui { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+        public string Direccion { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+
+        public string? TipoSangre { get; set; }
+
+        // EN KG, puede venir null
+        public decimal? Peso { get; set; }
+
+        // EN METROS, puede venir null (1.75, 1.60, etc.)
+        public decimal? Altura { get; set; }
+    }
+}
